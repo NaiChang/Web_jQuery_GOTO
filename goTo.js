a5f5781adee10717d04f181e694397e6a6d68985
NaiChang <nainai0123@gmail.com>
2020-10-29 18:51:19 +0800

    jq code-- "IF"

--- goTo.js
+++ goTo.js
@@ -6,8 +6,16 @@ $("*").each(function (index, element) {
         var target = $(this).attr("data-gt-target");
         var duration = $(this).attr("data-gt-duration");
         var offset = $(this).attr("data-gt-offset");
-        console.log("目標 :" + target);
-        console.log("時間 :" + duration);
-        console.log("位移 :" + offset);
+
+        // JS 語法 : 判斷式
+        //  if (條件) {程式區塊}
+        // 當條件成立，會執行程式區塊
+
+        // 如果 目標有資料 才會執行 { } 內的程式
+        if (target) {
+            console.log("目標 :" + target);
+            console.log("時間 :" + duration);
+            console.log("位移 :" + offset);
+        }
     });
 });
