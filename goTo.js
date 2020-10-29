af99806278016681e9d096bd8451a89dbb727151
NaiChang <nainai0123@gmail.com>
2020-10-29 19:00:48 +0800

    target-offset

--- goTo.js
+++ goTo.js
@@ -12,10 +12,16 @@ $("*").each(function (index, element) {
         // 當條件成立，會執行程式區塊
 
         // 如果 目標有資料 才會執行 { } 內的程式
+        // 避免出現 undefine (未定義 -不存在的資料)
+        // 可在網頁的"檢查-console"檢查
         if (target) {
             console.log("目標 :" + target);
             console.log("時間 :" + duration);
             console.log("位移 :" + offset);
+
+            // 上方位置 = 目標區塊.位移().上方的位置
+            $(target).offset().top;
+            console.log("要前往元素的上方位置:" + top)
         }
     });
 });
